8a54a50e234f3b118e7359a79f745a2a297b4331
Your Ajinkya2601 <ajinkyachitre51@gmail.com>
2024-06-13 21:50:04 +0530

    complete

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import GlobalStyle from './components/GlobalStyle';
+import { useEffect } from 'react';
+import AboutUs from './pages/AboutUs';
+import Nav from './components/Nav';
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import { Routes, Route,useLocation } from 'react-router-dom';
+import MovieDetail from './pages/MovieDetail';
+import { AnimatePresence } from 'framer-motion';
+
+function App() {
+  const location = useLocation();
+  
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Nav />
+      <AnimatePresence mode='wait'>
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<AboutUs />} />
+        <Route path="/work" element={<OurWork />} />
+        <Route path='/work/:id' element={<MovieDetail/>}></Route>
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default App;
